fc2d6e4553a5db944950587e60c82904bcd88e8f
abysssal <gamerlol5epic@gmail.com>
2023-03-03 16:45:06 -0500

    init

---
+++ core/src/com/mygdx/game/catGame.java
@@ -0,0 +1,38 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+// <summary>
+// Idea:
+// cat game where you deliver coffee to cats first thing in the morning, but you are also tired so you may have to drink
+// your customer's coffee to gain energy
+// if you fall asleep completely, the game is over
+// </summary>
+
+public class catGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
